7c4a880c4dd1c1584efb209212e0ad41f85d032c
Chulshin Kim <ckim349@aucklanduni.ac.nz>
2024-05-01 12:02:30 +1200

    feat: dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
+import useLocalStorage from "use-local-storage";
+
 import Document from './components/Document'
+import ToggleDarkMode from './components/ToggleDarkMode';
 
 function App() {
+  const preference = window.matchMedia("(prefers-color-scheme: dark)").matches;
+  const [isDark, setIsDark] = useLocalStorage("isDark", preference);
+
   return (
-    <div className='container'>
+    <div className='container' data-theme={isDark ? "dark" : "light"}>
+      <ToggleDarkMode handleChange={() => setIsDark(!isDark)} isChecked={isDark}/>
       <Document />
     </div>
   );
---
+++ src/components/ToggleDarkMode.tsx
@@ -0,0 +1,15 @@
+const ToggleDarkMode = ({ handleChange, isChecked }) => {
+  return (
+    <div className="toggle-container">
+      <input
+        type="checkbox"
+        id="check"
+        className="toggle"
+        onChange={handleChange}
+        checked={isChecked}
+      />
+      <label htmlFor="check">Dark Mode</label>
+    </div>
+  );
+};
+export default ToggleDarkMode
