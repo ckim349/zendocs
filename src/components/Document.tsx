e7e9716e63776a868799f2375b597f2f0c061716
Chulshin Kim <ckim349@aucklanduni.ac.nz>
2024-05-04 20:02:49 +1200

    feat: temporary collaborative editing

--- src/components/Document.tsx
+++ src/components/Document.tsx
@@ -11,6 +11,9 @@ import StarterKit from '@tiptap/starter-kit'
 import Underline from '@tiptap/extension-underline'
 import Highlight from '@tiptap/extension-highlight'
 import TextAlign from '@tiptap/extension-text-align'
+import Collaboration from '@tiptap/extension-collaboration'
+import * as Y from 'yjs'
+import { TiptapCollabProvider } from '@hocuspocus/provider'
 
 import Drawing from "./DrawingExtension"
 import SmilieReplacer from './SmilieReplacer'
@@ -18,6 +21,27 @@ import TextEditor from './TextEditor'
 import Toolbar from './Toolbar'
 
 const Document = () => {
+  const doc = new Y.Doc();
+
+  const provider = new TiptapCollabProvider({
+    name: "document.name", // Unique document identifier for syncing. This is your document name.
+    appId: '7j9y6m10', // Your Cloud Dashboard AppID or `baseURL` for on-premises
+    token: 'notoken', // Your JWT token
+    document: doc,
+
+    onSynced() {
+
+      if( !doc.getMap('config').get('initialContentLoaded') && editor ){
+        doc.getMap('config').set('initialContentLoaded', true);
+  
+        editor.commands.setContent(`
+        <p>
+          goon be gooning
+        </p>
+        `)
+      }
+  }})
+
   const editor = useEditor({
     extensions: [
       Color.configure({ types: [TextStyle.name, ListItem.name] }),
@@ -31,6 +55,7 @@ const Document = () => {
           keepMarks: true,
           keepAttributes: false,
         },
+        history: false,
       }),
       Typography,
       SmilieReplacer,
@@ -45,6 +70,9 @@ const Document = () => {
       }),
       TextAlign.configure({
         types: ['heading', 'paragraph'],
+      }),
+      Collaboration.configure({
+        document: doc
       }),
     ],
     content: ``,
