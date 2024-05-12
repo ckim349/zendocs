65ba45933dfee8bbc0cb831f9bcf6f15f9c3ff40
Chulshin Kim <ckim349@aucklanduni.ac.nz>
2024-05-12 12:54:33 +1200

    fix: dark mode button hover interaction

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -3,7 +3,13 @@ import { Link } from "react-router-dom"
 const HomePage = () => {
   return (
     <div>
+      <div className="search-bar">
+        <></>
+      </div>
       <Link to="/document">New Document</Link>
+      <div className="recent-documents">
+        <></>
+      </div>
     </div>
   )
 }
