bb42e2de8122e4897a308a14f3f4bcda48da6da8
Chulshin Kim <ckim349@aucklanduni.ac.nz>
2024-05-22 11:26:25 +1200

    fix: document load

--- server/controllers/documentController.ts
+++ server/controllers/documentController.ts
@@ -42,15 +42,19 @@ exports.document_update_post = [
 
       // Finds the document we want to update and then applies and saves the update
       const docToUpdate = await Document.findOne({ documentId: req.body.documentId }).exec();
+      if (!docToUpdate) {
+        return res.status(404).json({ error: 'Document not found' });
+      }
 
+      // Only applies update of content from database doc if content not null
       const doc = new Y.Doc();
-      // TODO not sure if we need to do a double update or if just the outer update is fine
-      // Y.applyUpdate(doc, toUint8Array(docToUpdate.content));
+      if (docToUpdate.content !== "AA==") {
+        Y.applyUpdate(doc, toUint8Array(docToUpdate.content));
+      }
       Y.applyUpdate(doc, update);
 
-      // TODO currently broken
-      // docToUpdate.content = fromUint8Array(Y.encodeStateAsUpdate(doc))
-      // docToUpdate.save();
+      docToUpdate.content = fromUint8Array(Y.encodeStateAsUpdate(doc))
+      await docToUpdate.save();
 
       res.json({ success: true });
     }
@@ -65,15 +69,18 @@ exports.document_load_get = [
       console.log('errors :c')
       return;
     } else {
-      // const docToFind = await Document.findOne({ documentId: "b34f778d-2f40-41a0-8cb4-7502999ca4b7" }).exec();
       const docToFind = await Document.findOne({ documentId: req.params.id }).exec();
-
-      if (docToFind === null) {
-        console.log('error finding document');
-        return;
+      if (!docToFind) {
+        return res.status(404).json({ error: 'Document not found' });
       }
 
-      res.json({ docToFind });
+      // Only applies update of content from database doc if content not null
+      const doc = new Y.Doc();
+      if (docToFind.content !== "AA==") {
+        Y.applyUpdate(doc, toUint8Array(docToFind.content));
+      }
+
+      res.send({ docToFind });
     }
   }),
 ];
