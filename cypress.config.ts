168760a2d2f397bdcdf06b1b218d1d39108ca09e
Chulshin Kim <ckim349@aucklanduni.ac.nz>
2024-05-29 21:18:18 +1200

    fix: increased wait timeout for cypress run

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,4 +4,10 @@ export default defineConfig({
   e2e: {
     baseUrl: "http://localhost:5173/",
   },
+  component: {
+    devServer: {
+      framework: "react",
+      bundler: "vite",
+    },
+  },
 });
